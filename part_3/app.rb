eb220afb84c9c3e8ee6d4c833593548279868968
Ravi Sharma <ravis4376@gmail.com>
2015-02-07 17:55:07 +0530

    added routes to the app

--- part_3/app.rb
+++ part_3/app.rb
@@ -3,10 +3,24 @@
 
 # the problem here is no matter what the request is it will always retutn the same response
 
+
 require 'controllers/root_controller'
 
 class App
 	def call(env)
-		RootController.new.show
+		path = env["REQUEST_PATH"]
+# this case structure will simply say that , it will parse the request path and if it finds that
+# it's equal to '/' it will show the RootController
+# if not the '/' , it will show a 404 status
+		case path
+		when %r{^/$}
+			RootController.new.show
+		else
+			[404,{"Content-Length"=>"9"},["Not Found"]]
+		end	
 	end
 end
+
+# if u want to add more controller or routes , u simply add them to the case
+
+# that's not the optimum way to route , in case of Rails the router is in the Gem called Journey
--- part_3/controllers/root_controller.rb
+++ part_3/controllers/root_controller.rb
@@ -1,3 +1,5 @@
+# we don't want to have these view code in our controller
+
 class RootController
 	def show
 		body = '<html><body><h1>Hello World</h1></body></html>'
